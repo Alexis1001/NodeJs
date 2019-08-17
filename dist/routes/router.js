cdc87678de3f52b781d3e1b30b4d8cfbd0152b26
PC <alexisperezgomez1@gmial.com>
2019-08-17 17:39:52 -0500

    apirest whit node.js mqtt

--- dist/routes/router.js
+++ dist/routes/router.js
@@ -1,7 +1,56 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'animales',
+    port: '3306'
+});
+connection.connect(function (error) {
+    if (error)
+        throw error;
+    else
+        console.log("conexion exitosa");
+});
 const router = express_1.Router();
+router.post('/api/animales', (req, res) => {
+    connection.query('insert into perro ( nombre, fecha) values(?,?)', [req.body.nombre, req.body.fecha], function (error, resultado) {
+        if (error)
+            throw error;
+        else {
+            res.json({
+                mensaje: 'perro creado'
+            });
+        }
+    });
+});
+router.get('/api/animales', (req, res) => {
+    connection.query('select * from perro', function (error, resultado) {
+        if (error)
+            throw error;
+        else {
+            res.json({
+                mensaje: resultado
+            });
+        }
+    });
+});
+//Fechas  /////
+router.get('/api/animales/:F1/:FF', (req, res) => {
+    connection.query('select * from perro where perro.fecha BETWEEN' + "'" + req.params.F1 + "'" + ' AND ' + "'" + req.params.FF + "'", function (error, resultado) {
+        if (error)
+            throw error;
+        else {
+            res.json({
+                mensaje: resultado
+            });
+        }
+    });
+});
+//Fechas //////
 router.get('/mensajes', (req, res) => {
     res.json({
         ok: true,
--- routes/router.ts
+++ routes/router.ts
@@ -1,6 +1,73 @@
 import { Router, Request, Response } from 'express';
 
+
+var mysql    = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'animales',
+  port :'3306'
+});
+
+connection.connect(function (error:any) {
+   
+    if(error)
+    throw error;
+    else
+    console.log("conexion exitosa");
+    
+});
+    
 const router = Router();
+
+
+router.post('/api/animales', (req: Request, res: Response)=>{
+   connection.query('insert into perro ( nombre, fecha) values(?,?)',
+   [req.body.nombre,req.body.fecha],
+   function(error:any ,resultado:any){
+    if(error)
+       throw error
+       else{
+           res.json({
+            mensaje:'perro creado'
+           });
+       }
+   })
+
+});
+
+router.get('/api/animales',(req: Request, res: Response)=>{
+   connection.query('select * from perro',
+   function(error:any ,resultado:any){
+    if(error)
+       throw error
+       else{
+           res.json({
+            mensaje: resultado
+           });
+       }
+   })
+
+});
+ 
+//Fechas  /////
+router.get('/api/animales/:F1/:FF',(req: Request, res: Response)=>{
+connection.query('select * from perro where perro.fecha BETWEEN'+"'"+req.params.F1 +"'" +' AND '+"'"+req.params.FF+"'",
+function(error:any ,resultado:any){
+     if(error)
+        throw error
+        else{
+            res.json({
+             mensaje: resultado
+            });
+        }
+    })
+ 
+ });
+
+//Fechas //////
+
 
 router.get('/mensajes',(req: Request, res: Response)=>{
     res.json({
